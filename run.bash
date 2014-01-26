4684088a8e77ba8010f728bb6368768fd2f6e1ef
nsf <no.smile.face@gmail.com>
2014-01-26 16:41:28 +0600

    Add example results and ldc2/gdc.

--- run.bash
+++ run.bash
@@ -6,8 +6,12 @@ echo -e "\n=== gcc -O3:"
 time ./bin_test_c_gcc > /dev/null
 echo -e "\n=== mono C#:"
 time ./bin_test_cs > /dev/null
-echo -e "\n=== D:"
-time ./bin_test_d > /dev/null
+echo -e "\n=== D (dmd):"
+time ./bin_test_d_dmd > /dev/null
+echo -e "\n=== D (ldc2):"
+time ./bin_test_d_ldc > /dev/null
+echo -e "\n=== D (gdc):"
+time ./bin_test_d_gdc > /dev/null
 echo -e "\n=== Go gc:"
 time ./bin_test_go_gc > /dev/null
 echo -e "\n=== Go gccgo -O3:"
