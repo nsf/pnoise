c05db5c9eeceab04114a88da9dd9528a1910819e
Edmund Smith <edmund.pd.smith@gmail.com>
2014-08-24 07:34:15 +0100

    Added SSE flags for GDC

--- compile.bash
+++ compile.bash
@@ -3,7 +3,7 @@ gcc -std=c99 -march=native -msse3 -mfpmath=sse -O3 -o bin_test_c_gcc test.c -lm
 clang -std=c99 -march=native -msse3 -mfpmath=sse -O3 -o bin_test_c_clang test.c -lm
 dmd -ofbin_test_d_dmd -O -noboundscheck -inline -release test.d
 ldc2 -O3 -ofbin_test_d_ldc test.d -release
-gdc -O3 -o bin_test_d_gdc test.d
+gdc -O3 -o bin_test_d_gdc test.d -frelease -msse3 -mfpmath=sse -finline
 gccgo -O3 -g -o bin_test_go_gccgo test.go
 go build -o bin_test_go_gc test.go
 rustc --opt-level 3 -o bin_test_rs test.rs
