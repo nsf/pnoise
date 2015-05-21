ac22e374619f595bcb6b88c4fbb21384221864cf
nsf <no.smile.face@gmail.com>
2015-05-22 00:57:16 +0500

    Fix rust compile option. Use -O for optimizations.

--- compile.bash
+++ compile.bash
@@ -6,7 +6,7 @@ ldc2 -O5 -ofbin_test_d_ldc test.d -release -mcpu=native -inline -boundscheck=off
 gdc -Ofast -o bin_test_d_gdc test.d -frelease -finline -march=native -fno-bounds-check
 gccgo -O3 -g -o bin_test_go_gccgo test.go
 go build -o bin_test_go_gc test.go
-rustc --opt-level 3 -o bin_test_rs test.rs
+rustc -O -o bin_test_rs test.rs
 mcs -out:bin_test_cs test.cs
 nim c -d:release --cc:gcc -o:bin_test_nim_gcc test.nim
 nim c -d:release --cc:clang -o:bin_test_nim_clang test.nim
