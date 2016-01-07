0e51b0645df049183636a069ced7b0279e353bde
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2016-01-07 00:20:03 -0800

    Fix compile.bash for LDC
    ``` 
       -O5                             - Link-time optimisation
    ```

--- compile.bash
+++ compile.bash
@@ -2,7 +2,7 @@
 gcc -std=c99 -march=native -msse3 -mfpmath=sse -O3 -o bin_test_c_gcc test.c -lm
 clang -std=c99 -march=native -msse3 -mfpmath=sse -O3 -o bin_test_c_clang test.c -lm
 dmd -ofbin_test_d_dmd -O -boundscheck=off -inline -release test.d
-ldc2 -O5 -ofbin_test_d_ldc test.d -release -mcpu=native -inline -boundscheck=off
+ldc2 -O -ofbin_test_d_ldc test.d -release -mcpu=native -inline -boundscheck=off
 gdc -Ofast -o bin_test_d_gdc test.d -frelease -finline -march=native -fno-bounds-check
 gccgo -O3 -g -o bin_test_go_gccgo test.go
 go build -o bin_test_go_gc test.go
